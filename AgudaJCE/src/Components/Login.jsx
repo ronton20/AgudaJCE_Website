917db1d89f60cad0265af965ea2b2e45647cd106
ronton20 <ronamarse@gmail.com>
2023-06-16 14:14:18 +0300

    changed login inputs to display a red border on invalid inputs

--- AgudaJCE/src/Components/Login.jsx
+++ AgudaJCE/src/Components/Login.jsx
@@ -8,8 +8,7 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase.js";
 
 function Login(props) {
-
-	const [ errorMessage, setErrorMessage ] = useState("");
+	const [errorMessage, setErrorMessage] = useState("");
 
 	const ids = {
 		email: "login_email",
@@ -18,15 +17,18 @@ function Login(props) {
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
+		const emailField = document.getElementById(`input_field_${ids.email}`);
+		const passwordField = document.getElementById(`input_field_${ids.password}`);
 		// clean the error if there is one
 		setErrorMessage("");
+		emailField.classList.remove("invalid");
+		passwordField.classList.remove("invalid");
 		// disable the button to prevent multiple submissions
 		const submitButton = e.target.querySelector(".submit_button");
 		submitButton.disabled = true;
-		
-		
-		const email = document.getElementById(`input_field_${ids.email}`).value;
-		const password = document.getElementById(`input_field_${ids.password}`).value;
+
+		const email = emailField.value;
+		const password = passwordField.value;
 		try {
 			// check if user is blocked
 			const querySnapshot = await getDocs(collection(db, "Users"));
@@ -47,12 +49,20 @@ function Login(props) {
 
 	const invalidUser = (errorMessage) => {
 		setErrorMessage(errorMessage);
+		document.getElementById(`input_field_${ids.email}`).classList.add("invalid");
+		document.getElementById(`input_field_${ids.password}`).classList.add("invalid");
 	};
 
 	return (
 		<div className="login_div">
 			<h1>{props.languageHelper.header}</h1>
-			{errorMessage === "" ? (<></>) : (<p id="login_error" className="error_message">{ errorMessage }</p>)}
+			{errorMessage === "" ? (
+				<></>
+			) : (
+				<p id="login_error" className="error_message">
+					{errorMessage}
+				</p>
+			)}
 			<form className="login_form" onSubmit={handleSubmit}>
 				<InputField _id={ids.email} label={props.languageHelper.email} type="email" />
 				<InputField
